72289a6eab5b49e0fe78db752b20027b74c02702
Ernesto <ernestoaguaysol@gmail.com>
2017-12-17 18:25:57 -0300

    separar las rutas hijas a un archivo especifico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    {
-        path: 'usuario/:id',
-        component: UsuarioComponent,
-        children: [
-            { path: 'nuevo', component: UsuarioNuevoComponent },
-            { path: 'editar', component: UsuarioEditarComponent },
-            { path: 'detalle', component: UsuarioDetalleComponent },
-            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-        ]
-    },
+    { path: 'usuario/:id', component: UsuarioComponent, children: USUARIO_ROUTES },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
